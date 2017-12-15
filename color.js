584227aba0ad5a3293e1c70780e34efd1ffb9625
osamagmt <34580348+osamagmt@users.noreply.github.com>
2017-12-15 17:04:32 +0000

    Update and rename README.md to color.js

---
+++ color.js
@@ -0,0 +1,30 @@
+كود الوان يسوي 140 لون 
+
+
+
+client.on('message', function(message) {
+                  if(!message.channel.guild) return;
+    if(message.content ===  'colors create') {
+        if(message.member.hasPermission('MANAGE_ROLES')) {
+            setInterval(function(){})
+            message.channel.send('جاري عمل الالوان |✅')
+        }else{
+            message.channel.send('ما معاك البرمشن المطلوب  |❌')
+            }
+    }
+});
+
+client.on('message', message=>{
+    if (message.content ===  'colors create'){
+              if(!message.channel.guild) return;
+            if (message.member.hasPermission('MANAGE_ROLES')){
+                setInterval(function(){})
+                  let count = 0;
+                  let ecount = 0;
+        for(let x = 1; x < 141; x++){
+            message.guild.createRole({name:x,
+            color: 'RANDOM'})
+            }
+            }
+    }
+});
